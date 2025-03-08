0fa28c23e2a555c278a62d0fc322715a9629f20a
gxy <2219570448@qq.com>
2025-03-08 19:47:55 +0800

    AOP 实现统一处理日志

---
+++ src/main/java/com/nowcoder/community/aspect/AlphaAspect.java
@@ -0,0 +1,51 @@
+package com.nowcoder.community.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * 文件名称: AlphaAspect.java
+ * 作者: gxy
+ * 创建日期: 2025/3/8
+ * 描述: AOP示例
+ */
+//@Component
+//@Aspect
+public class AlphaAspect {
+
+    // 定义切点
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before() {
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning() {
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+
+}
