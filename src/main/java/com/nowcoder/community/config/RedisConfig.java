ac1d0ecb8057b187bcbefa1d390fae887b5f7832
gxy <2219570448@qq.com>
2025-03-08 22:35:28 +0800

    Spring整合Redis示例

---
+++ src/main/java/com/nowcoder/community/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * 文件名称: RedisConfig.java
+ * 作者: gxy
+ * 创建日期: 2025/3/8
+ * 描述: Redis配置类
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // 设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        // 初始化操作，让配置生效
+        template.afterPropertiesSet();
+        return template;
+    }
+}
