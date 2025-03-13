70ff4caa93065b18265fa75b20f37ccc61cfc6cc
gxy <2219570448@qq.com>
2025-03-13 20:26:51 +0800

    显示系统消息通知

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/MessageInterceptor.java
@@ -0,0 +1,39 @@
+package com.nowcoder.community.controller.interceptor;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.MessageService;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 文件名称: MessageInterceptor.java
+ * 作者: gxy
+ * 创建日期: 2025/3/13
+ * 描述: 消息拦截器
+ */
+@Component
+public class MessageInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private MessageService messageService;
+
+    // 在模板渲染之前，将未读消息的数量传递给模板
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if(user != null && modelAndView != null) {
+            int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+            int noticeUnreadCount = messageService.findNoticeUnreadCount(user.getId(), null);
+            modelAndView.addObject("allUnreadCount", letterUnreadCount + noticeUnreadCount);
+        }
+    }
+}
